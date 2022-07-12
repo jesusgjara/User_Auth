7687642ca3d51f33facccf14eefa278de4ec0796
Jesus G Jara <jesusgjara.1@gmail.com>
2022-07-11 21:00:07 -0400

    Creating folders, files, creating schema

---
+++ Auth/Auth.js
@@ -0,0 +1,20 @@
+const User = require('../model/User')
+
+exports.register = async (req, res, next) => {
+    const {username, password} = req.body
+    if (password.length < 6) {
+        return res.status(400).json({message: "password less than 6 characters"})
+    }
+    try {
+        await User.create({
+            username,
+            password,
+        }).then(user => res.status(200).json({message: "User successfully created", user})
+        ) 
+    } catch(err) {
+        res.status(401).json({
+            message: "User creation not successfull",
+            error: error.message,
+        })
+    }
+}
