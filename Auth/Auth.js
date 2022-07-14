efce5ee6b3c32a2be31e57a62f3ac424921c52b5
Jesus G Jara <jesusgjara.1@gmail.com>
2022-07-14 00:59:47 -0400

    Setting routes for User update and deleteUser

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -46,3 +46,65 @@ exports.login = async (req, res, next) => {
         })
     }
 }
+
+exports.update = async (req, res, next) => {
+    const {role, id} = req.body
+    if (role && id) {
+        if (role === "admin") {
+            await User.findById(id)
+                .then((user) => {
+                    if (user.role !== "admin") {
+                        user.role = role
+                        user.save((err) => {
+                            if (err) {
+                                res.status(400).json({
+                                    message: "An error occurred",
+                                    error: err.message
+                                })
+                                process.exit(1)
+                            }
+                            res.status(201).json({
+                                message: "update successful",
+                                user
+                            })
+                        })
+                    } else {
+                        res.status(400).json({
+                            message: "User is already an Admin"
+                        })
+                    }
+                })
+                .catch((error) => {
+                    res.status(400).json({
+                        message: "an error happened",
+                        error: error.message
+                    })
+                })
+        } else {
+            res.status(400).json({
+                message: "Role is not admin",
+            })
+        }
+    } else {
+        res.status(400).json({
+            message: "Role or ID is missing"
+        })
+    }
+}
+
+exports.deleteUser = async (req, res, next) => {
+    const {id} = req.body
+    await User.findById(id)
+        .then(user => user.remove())
+        .then(user => {
+            res.status(201).json({
+                message: "User successfully deleted", user
+            })
+        })
+        .catch(error => {
+            res.status(400).json({
+                message: "an error occurred",
+                error: error.message
+            })
+        })
+}
