7cf732cbfbfcc624fc45b91b9e20becf6b0b8822
Jesus G Jara <jesusgjara.1@gmail.com>
2022-07-16 23:28:54 -0400

    Creating token for users, adding cookie-parser

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,25 +1,46 @@
 const User = require('../model/User')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const TOKEN = process.env.JWT_TOKEN
 
 exports.register = async (req, res, next) => {
-    const {username, password} = req.body
+    const { username, password } = req.body;
     if (password.length < 6) {
-        return res.status(400).json({message: "password less than 6 characters"})
+      return res.status(400).json({ message: "Password less than 6 characters" });
     }
-    try {
-        bcrypt.hash(password, 10).then(async (hash) =>
-        await User.create({
-            username,
-            password: hash,
-        }).then(user => res.status(200).json({message: "User successfully created", user})
-        )) 
-    } catch(err) {
-        res.status(401).json({
-            message: "User creation not successfull",
-            error: error.message,
+    bcrypt.hash(password, 10).then(async (hash) => {
+      await User.create({
+        username,
+        password: hash,
+      })
+        .then((user) => {
+          const maxAge = 3 * 60 * 60;
+          const token = jwt.sign(
+            { id: user._id, username, role: user.role },
+            TOKEN,
+            {
+              expiresIn: maxAge, // 3hrs
+            }
+          );
+          res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000,
+          });
+          res.status(201).json({
+            message: "User successfully created",
+            user: user._id,
+            role: user.role,
+          });
         })
-    }
-}
+        .then(userTokenCreation())
+        .catch((error) =>
+          res.status(400).json({
+            message: "User not successfully created",
+            error: error.message,
+          })
+        );
+    });
+  };
 
 exports.login = async (req, res, next) => {
     const{username, password} = req.body
@@ -36,15 +57,32 @@ exports.login = async (req, res, next) => {
             error: "User not found",
         })
     } else {
+        //comparing given password with hashed password
         bcrypt.compare(password, user.password).then(function(result){
-        result ? res.status(200).json({
-            message: "login successful",
-            user,
-        })
-        : res.status(400).json({message: "Invalid login"})
-    })
-    }
-    } catch {
+            if (result) {
+                const maxAge = 3 * 60 * 60;
+                const token = jwt.sign(
+                  { id: user._id, username, role: user.role },
+                  jwtSecret,
+                  {
+                    expiresIn: maxAge, // 3hrs in sec
+                  }
+                );
+                res.cookie("jwt", token, {
+                  httpOnly: true,
+                  maxAge: maxAge * 1000, // 3hrs in ms
+                });
+                res.status(201).json({
+                  message: "User successfully Logged in",
+                  user: user._id,
+                });
+              } else {
+                res.status(400).json({ message: "Login not succesful" });
+              }
+            });
+          
+        }
+    } catch (error) {
         res.status(400).json({
             message: "An error happened, I dunno",
             error: error.message,
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./db')
-
+const cookieParser = require('cookie-parser')
 connectDB()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/Auth', require('./Auth/Route'))
 
